310b6fff4996105b348bdc51f6cba0b40714d9b1
kushalguha5 <64835424+kushalguha5@users.noreply.github.com>
2020-11-03 01:45:15 +0530

    Create func_header.cpp

---
+++ func_header.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+
+
+  int story_premise(); 
+
+  int variables();
+
+int level_1();
+
+int walk_lv1(int score);
+int car_lv1(int score);
+
+  
+int train_lv1(int score);
+int bullet_lv1(int score);
+int plane_lv2(int score);
+int hotballon_lv2(int score);
+int glyder_lv2(int score);
+int parachute_lv2(int score);
+int cruise_lv3(int score);
+int speedboat_lv3(int score);
+int boat_lv3(int score);
+int canoe_lv3(int score);
+int rocket_lv(int score);
+int blackhole_lv(int score);
+int donkey_lv(int score);
+
